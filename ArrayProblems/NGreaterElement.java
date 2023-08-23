3694340956262565f1007bda4c2e2416f2a0a038
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-08-23 07:56:29 +0530

    NextGraterElement Solution Addedgit status :)

--- ArrayProblems/NGreaterElement.java
+++ ArrayProblems/NGreaterElement.java
@@ -1,23 +1,28 @@
 package practiceSet.ArrayProblems;
+import java.util.Arrays;
 import java.util.Stack;
 public class NGreaterElement {
     public static void main(String[] args) {
-        int[] arr = {2,4,8,90,77,84};
-        findGreater(arr);
+        int[] arr = {2,4,8,90,77,84};     
+        System.out.println(Arrays.toString(findGreater(arr)));
     }
-    public static void findGreater(int[] arr)
+    public static int[] findGreater(int[] arr)
     {
-        //  use stack
-        Stack<Integer> stack = new Stack<>(); // store in increasing order
-        int n = arr.length;
-        for(int i=n;i>=0;i--)
-        {
-            if(!stack.isEmpty()){
-            while( (i<n )&& (arr[i] > stack.peek())) stack.pop(); //remove small elemnets in stack
-            }
-            else arr[i] =stack.peek();
-            stack.add(arr[i]);
-        }
+       int[] ans = new int[arr.length];
+       Stack<Integer> st = new Stack<>();
+       for(int i=arr.length-1;i>=0;i--)
+       {
+        while(!st.isEmpty() && st.peek() <= arr[i]) st.pop();
 
+        if(!st.isEmpty())
+        {
+            ans[i] = st.peek();
+        }else{
+            ans[i] = -1;
+        }
+        st.push(arr[i]);
+
+       }
+       return ans;
     }
 }
